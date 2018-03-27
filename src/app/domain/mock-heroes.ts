9b87d863ccecfc0574bca4ce5daaf9fdf670e794
Caroline <carolinebaptista41@gmail.com>
2018-03-26 21:28:52 -0300

    Herois

---
+++ src/app/domain/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from "./hero";
+
+export const HEROES : Hero[] = [
+    {id: 11, name: 'Superman'},
+    {id: 12, name: 'Ficou Fraco'},
+    {id: 13, name: 'O Pinguim'},
+    {id: 14, name: 'Jogou Kriptonita'},
+    {id: 15, name: 'Batman'},
+    {id: 16, name: 'Mulher Maravilha'},
+    {id: 17, name: 'Super Poderosas'}
+]
